3235a7f85aa6c347f7dd1609cfc897f951a525e3
Dehvon Curtis <dcurtis@dcurtis-macos.corp.adobe.com>
2020-01-02 13:38:10 -0700

    fixed error in script

---
+++ vault-unseal-login.py
@@ -0,0 +1,20 @@
+# python3
+# by: dehvon curtis
+import os
+
+os.system("cat /tmp/docker-vault-keys.txt | grep Key")
+os.system("cat /tmp/docker-vault-keys.txt | grep Token")
+
+token1 = input("Please input first unseal key > ")
+token2 = input("Please input second unseal key > ")
+token3 = input("Please input third unseal key > ")
+
+os.system(f"vault operator unseal {token1}")
+os.system(f"vault operator unseal {token2}")
+os.system(f"vault operator unseal {token3}")
+
+roottoken = input("Please input root token > ")
+os.system(f"vault login {roottoken}")
+
+print("enabling kv secrets and creating path")
+os.system("vault secrets enable -path=secret/ kv")
